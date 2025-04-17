6f163b05df4c8d83426abe6ebadf7c7664d8e08c
Hiago Silva Damasceno <161268387+HiagoDamasceno@users.noreply.github.com>
2025-04-16 21:01:13 -0300

    Update test_math_utils.py

--- test_math_utils.py
+++ test_math_utils.py
@@ -1,18 +1,18 @@
-from math_utils import add, subtract
+from math_utils import add, subtract, multiply
 
 def test_add():
     assert add(2, 3) == 5
 
 def test_subtract():
     assert subtract(5, 2) == 3
-
+    
 def test_multiply():
     a, b = 2, 3
     if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
         raise ValueError("Both arguments must be numbers")
-    assert  a * b == 6
+    assert a * b == 6
 
     try:
-        test_multiply(0, "string")
+        multiply(0, "string")
     except ValueError:
         assert True
